9437cf66154f37e25fde76c65f851daeaf639310
shadid12 <shadidhaque2012@gmail.com>
2020-04-20 01:32:21 -0400

    list posts done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withAuthenticator } from 'aws-amplify-react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listPosts } from './graphql/queries';
+
+import './app.css';
 
 function App() {
+
+  const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    getAllPosts()
+  }, [])
+
+  const getAllPosts = async () => {
+    try {
+      let allposts = await API.graphql(graphqlOperation(listPosts));
+      console.log('all listPosts', allposts)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav className="nav">
+        <a><b>Home</b></a>
+      </nav>
+      <div class="row">
+        <div className="column column-50">
+          <h2>Posts</h2>
+          <ul>
+            <li>
+              <a>View This article</a>
+            </li>
+            <li>
+              <a>View This article</a>
+            </li>
+          </ul>
+        </div>
+        <div className="column column-offset-25">
+          <button>Write Post</button>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
